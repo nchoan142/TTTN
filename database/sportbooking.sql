28ee5a2bdf98768b76fb68ae929804a4f0586ffa
Nguyen Cong Hoan <nchoan142@gmail.com>
2026-04-06 07:12:15 +0700

    modify database

--- database/sportbooking.sql
+++ database/sportbooking.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `bookings`
 --
 
+CREATE DATABASE IF NOT EXISTS sport_db;
+USE sport_db;
+
 DROP TABLE IF EXISTS `bookings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
